2a49ad73e8a70734e9feb03abd2047578e685a20
Rawiya-lou <raoiua15@gmail.com>
2025-06-01 10:35:48 +0100

    basic to-do list where users can add tasks, mark them as complete, and delete them. The tasks are stored in local storage so they persist across browser sessions.

--- script.js
+++ script.js
@@ -2,48 +2,93 @@ const input = document.getElementById('input-box');
 const taskContainer = document.getElementById('task-container');
 const addBtn = document.getElementById('add-btn');
 
+//empty arr to store tasks objects
+let tasks = [];
 
-addBtn.addEventListener('click', addTask);
-input.addEventListener('keydown', (e) => {
-    if(e.key === 'Enter'){
-        addTask();
+function addTasks() {
+    const taskText = input.value.trim() ;
+    if(taskText === ''){
+        alert('You need to add a task');
+        return;
     }
-
-})
-
-function addTask() {
-    if(input.value === ''){
-        alert('You have to Add task');
-    }else{
-        const newTask = document.createElement('li');
-        newTask.textContent = input.value;
-        taskContainer.appendChild(newTask);
-        const span = document.createElement('span');
-        span.innerHTML = '\u00d7'; 
-        newTask.appendChild(span);
+    const existTasks = new Set(tasks.map(task => task.text));
+    if(existTasks.has(taskText)){
+        alert('This task already exists');
+        input.value = '';
+        return;
     }
-    input.value = '';
+        tasks.push(
+            {id: Date.now(),
+             text: taskText}
+        );
+        
+    renderTasks();
     saveData();
+    input.value = '';
 }
 
+function renderTasks() {
+    taskContainer.innerHTML = '';
+     tasks.forEach((task, index) => {
+        const li = document.createElement('li');
+        li.textContent = task.text;
+        li.id = task.id;
+        taskContainer.appendChild(li);
+
+        const span = document.createElement('span');
+        span.textContent = '\u00d7';
+        span.setAttribute('aria-label', 'Delete task');
+        li.appendChild(span);
+     });
+
+    
+}
+
+
 taskContainer.addEventListener('click', (e) =>{
+   
     if(e.target.tagName === 'LI'){
         e.target.classList.toggle('checked');
         saveData();
 
     }else if(e.target.tagName === 'SPAN'){
-        e.target.parentElement.remove();
+    
+        const id = e.target.parentElement.id;
+    
+        const taskToRemove = tasks.findIndex(task => task.id === parseInt(id, 10));
+
+        if(taskToRemove !== -1){
+            tasks.splice(taskToRemove, 1)
+            e.target.parentElement.remove();
+        }
+        
         saveData();
     }
 });
 
-
-function saveData(){
-    localStorage.setItem('data', taskContainer.innerHTML);
+function saveData() {
+    localStorage.setItem('data', JSON.stringify(tasks));
 }
 
-function showTask(){
-  taskContainer.innerHTML = localStorage.getItem('data');
-}
+function retrieveData() {
+    const storedTasks = localStorage.getItem('data');
+    try{
 
-showTask();
+        tasks = storedTasks ? JSON.parse(storedTasks) : [];
+        renderTasks();
+    }catch(error) {
+        console.error('Error parsing JSON from localStorage', error);
+        tasks = [];
+    }
+    renderTasks();
+
+
+}
+retrieveData();
+
+addBtn.addEventListener('click', addTasks);
+input.addEventListener('keydown', (e) => {
+    if(e.key === 'Enter'){
+        addTasks();
+    }
+})
